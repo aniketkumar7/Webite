a4e5849fadc343f63a99e9e2041838456902c79b
Aniket Kumar <93782287+aniketkumar7@users.noreply.github.com>
2023-12-03 20:27:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 var toggles = document.querySelectorAll(".s5-top");
+
 toggles.forEach(function (toggle) {
+  
   toggle.addEventListener("click", function () {
+    
     var content = this.nextElementSibling;
     content.style.display = content.style.display === "none" ? "block" : "none";
 
     var title = this.querySelector("h3");
     var icon = this.querySelector(".toggle");
+    
     if (title.style.color === "black") {
       title.style.color = "rgb(130, 71, 255)";
       icon.innerHTML =
         "<i class='fa-solid fa-circle-xmark' style='color: ;'></i>";
-    } else {
+    } 
+      
+    else {
       title.style.color = "black";
       icon.innerHTML = "<i class='fa-solid fa-plus' style='color: ;'></i>";
     }
+    
   });
 });
 
 // function to change the body background color on scroll
-
 window.onscroll = function () {
   myFunction();
 };
+
 
 function myFunction() {
   if (
